e071554f2a6ad97fd532df8a410bab1ea0f8e9d8
VinayVR26 <vinayvanjre@gmail.com>
2023-12-24 22:10:51 +0800

    Deploy

--- src/scenes/StockOverview/Overview.jsx
+++ src/scenes/StockOverview/Overview.jsx
@@ -28,6 +28,13 @@ const Overview = ({ symbol, currency, filter }) => {
     return isMarketOpen;
   }
 
+  const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+
 
   const calculateDateValueForFilter = (filter) => {
     const now = new Date();
